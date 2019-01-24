e18cd59c1a84a4e4e6d63426783fb466280d0401
LAPTOP-5LF2RRRC\user <mor2222@naver.com>
2019-01-24 22:07:55 +0900

    edit android mapper

--- src/main/java/org/sopt/befit/mapper/ClosetMapper.java
+++ src/main/java/org/sopt/befit/mapper/ClosetMapper.java
@@ -33,17 +33,17 @@ public interface ClosetMapper {
     Closet findByClosetIdx(@Param("closet_idx") final int closet_idx);
 
     // 나의 옷장 리스트 조회
-    @Select("select cwp.closet_idx, b.name_korean, b.name_english, cwp.name, cwp.image_url, cwp.product_category_index, cwp.product_size, cwp.measure\n" +
+    @Select("select cwp.closet_idx, b.name_korean, b.name_english, cwp.name, cwp.image_url, cwp.product_category_idx, cwp.product_size, cwp.measure\n" +
             "    from brand as b,\n" +
             "            (select (closet.idx) as closet_idx, p.*, closet.product_size from product as p,\n" +
             "            (select uc.idx, uc.product_idx, uc.product_size from user_closet as uc\n" +
             "    where uc.user_idx = #{user_idx}) as closet where p.idx = closet.product_idx) as cwp\n" +
-            "    where cwp.product_category_index = #{category_idx} and cwp.brand_idx = b.idx order by cwp.closet_idx desc")
+            "    where cwp.product_category_idx = #{category_idx} and cwp.brand_idx = b.idx order by cwp.closet_idx desc")
     List<ClosetReq> getClosetProduct(@Param("user_idx") final int user_idx,
                                      @Param("category_idx") final int category_idx);
     
     // 나의 옷장 특정 아이템 조회
-    @Select("select cwp.closet_idx, b.name_korean, b.name_english, cwp.name, cwp.image_url, cwp.product_category_index, cwp.product_size, cwp.measure\n" +
+    @Select("select cwp.closet_idx, b.name_korean, b.name_english, cwp.name, cwp.image_url, cwp.product_category_idx, cwp.product_size, cwp.measure\n" +
             "    from brand as b,\n" +
             "            (select (closet.idx) as closet_idx, p.*, closet.product_size from product as p,\n" +
             "            (select uc.idx, uc.product_idx, uc.product_size from user_closet as uc\n" +
@@ -53,13 +53,13 @@ public interface ClosetMapper {
                                    @Param("closet_idx") final int closet_idx);
 
     // 옷장 아이템 등록 시 상품 검색 - 브랜드명 + 카테고리
-    @Select("SELECT * FROM product WHERE brand_idx = #{brand_idx} and product_category_index = #{category_idx} " +
+    @Select("SELECT * FROM product WHERE brand_idx = #{brand_idx} and product_category_idx = #{category_idx} " +
             "order by product.name desc")
     List<Products> getProductByBrandAndCategory (@Param("brand_idx") final int brand_idx,
                                            @Param("category_idx") final int category_idx);
 
     // 나의 옷장 아이템과 나의 선택 상품 사이즈 비교
-    @Select("select p.product_category_index, p.measure, uc.product_size from user_closet as uc, product as p where uc.idx = #{closet_idx} and p.idx = uc.product_idx")
+    @Select("select p.product_category_idx, p.measure, uc.product_size from user_closet as uc, product as p where uc.idx = #{closet_idx} and p.idx = uc.product_idx")
     SizeCompareReq getCompareMyProduct (@Param("user_idx") final int user_idx,
                                         @Param("closet_idx") final int closet_idx);
 
